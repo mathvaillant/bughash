c19becee8ad4762f89ad8f78bcf5268f302d928a
mathvaillant <matheusvaillant505@gmail.com>
2022-04-08 00:48:54 +0100

    chore: updates on sidemenu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,16 +1,19 @@
 import React, { useCallback } from 'react';
-import { IconButton } from "@mui/material";
+import { Avatar, Button, IconButton, Tooltip } from "@mui/material";
 import classNames from "classnames";
-import VideoFileIcon from '@mui/icons-material/VideoFile';
+import useToggle from "../../utils/hooks/useToggle";
+import './SideMenu.scss';
+import { useNavigate } from "react-router";
+import HomeIcon from '@mui/icons-material/Home';
 import Open from '@mui/icons-material/KeyboardArrowRight';
 import Close from '@mui/icons-material/KeyboardArrowLeft';
 import useEventListener from "../../utils/hooks/useEventListener.js";
-import CodeIcon from '@mui/icons-material/Code';
-import useToggle from "../../utils/hooks/useToggle";
-import './SideMenu.scss';
+import ListIcon from '@mui/icons-material/List';
+import ProfileMe from '../../assets/images/profile.png';
 
 const SideMenu: React.FC = () => {
   const [expanded, setExpanded] = useToggle();
+  const navigate = useNavigate();
 
   const handleClickAway = useCallback((e) => {
     const shouldResize = !e.target.closest("[data-sidebar-section]");
@@ -22,6 +25,43 @@ const SideMenu: React.FC = () => {
 
   useEventListener('click', handleClickAway);
 
+  const navigateToPath = (e: React.MouseEvent<HTMLButtonElement>, path: string): void => navigate(path);
+  
+  const getHomeButton = (): JSX.Element => {
+    if(!expanded) {
+      return <Tooltip placement={'right'} title={'Dashboard'}>
+        <IconButton onClick={(e) => navigateToPath(e, '/dashboard')}><HomeIcon /></IconButton>
+      </Tooltip>
+    }
+    return <Button onClick={(e) => navigateToPath(e, '/dashboard')} startIcon={<HomeIcon/>}>Dashboard</Button>
+  };
+
+  const getBugListButton = (): JSX.Element => {
+    if(!expanded) {
+      return <Tooltip placement={'right'} title={'Bugs List'}>
+        <IconButton onClick={(e) => navigateToPath(e, '/list')}><ListIcon /></IconButton>
+      </Tooltip>
+    }
+    return <Button onClick={(e) => navigateToPath(e, '/list')} startIcon={<ListIcon/>}>Bugs List</Button>
+  };
+
+  const getUserAvatar = (): JSX.Element => {
+    if(!expanded) {
+      return <Tooltip placement={'right'} title={'Profile'}>
+        <Avatar className={'UserAvatar'} alt="Matheus Vaillant" src={ProfileMe} sx={{ width: 30, height: 30 }}/>
+      </Tooltip>
+    }
+    return <Button 
+      className={'UserAvatar'}
+      onClick={() => console.log('a')} 
+      startIcon={
+        <Avatar alt="Remy Sharp" src={ProfileMe} sx={{ width: 34, height: 34 }}/>
+      }
+    >
+      Profile
+    </Button>
+  };
+
   return (
     <div className={classNames('SideMenu', {open: !expanded, closed: expanded})} data-sidebar-section='toggleClose'>
       <IconButton className='Sidebar__expandButton' onClick={setExpanded}>
@@ -29,7 +69,10 @@ const SideMenu: React.FC = () => {
         !expanded ? <Open /> : <Close />
       }
       </IconButton>
-      <IconButton><CodeIcon /></IconButton>
+
+      {getHomeButton()}
+      {getBugListButton()}
+      {getUserAvatar()}
     </div>
   )
 }
